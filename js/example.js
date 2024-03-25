7365453f8469b58aba0005e23b3f90a16359e86d
Jason Tran <159515554+JayTScripts@users.noreply.github.com>
2024-03-25 17:01:06 -0400

    add example page

---
+++ js/example.js
@@ -0,0 +1,14 @@
+const openBtn = document.querySelector(".js-card-opener");
+let song = new Audio("../media/birthday_song.mp3");
+let audioPlaying = false;
+
+openBtn.onclick = function () {
+  document.body.classList.toggle("open");
+  if (!audioPlaying){
+    song.play();  
+  } else {
+    song.pause();
+    song.currentTime = 0;
+  }
+  audioPlaying = !audioPlaying;
+};
